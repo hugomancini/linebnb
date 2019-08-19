9c84ca4c3e05cb44f72d852fa2ae05d9c048a710
maya douadi <maya.douadi@outlook.fr>
2019-08-20 00:33:11 +0200

    Start Style ProfilShow without Back

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_19_144219) do
+ActiveRecord::Schema.define(version: 2019_08_19_200851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -63,6 +63,7 @@ ActiveRecord::Schema.define(version: 2019_08_19_144219) do
     t.boolean "is_q"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "description"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Queuer.destroy_all
-User.destroy_all
 Review.destroy_all
 Reservation.destroy_all
+Queuer.destroy_all
+User.destroy_all
 
 url = "https://source.unsplash.com/random"
 15.times do
-  user = User.new(name: Faker::Name.name, email:Faker::Internet.email, password: "password", is_q: false, rating: rand(1..5))
+  desco = ""
+  rand(2..5).times do
+    desco << Faker::Lorem.sentence
+  end
+
+  user = User.new(name: Faker::Name.name, email:Faker::Internet.email, password: "password", is_q: false, rating: rand(1..5), description: desco)
   sleep(2)
   user.remote_picture_url = url
   user.save
 end
 
 15.times do
-  usere = User.new(name: Faker::Name.name, email:Faker::Internet.email, password: "password", is_q: true, rating: rand(1..5))
+  desc = ""
+  rand(2..5).times do
+    desc << Faker::Lorem.sentence
+  end
+  usere = User.new(name: Faker::Name.name, email:Faker::Internet.email, password: "password", is_q: true, rating: rand(1..5), description: desc)
   sleep(2)
   usere.remote_picture_url = url
   usere.save
 
-  queuer = Queuer.new(address: Faker::Address.full_address, description: Faker::Lorem.sentence, radius: rand(5...80), price_per_hour: rand(5...40), rating: rand(1..5))
+  desca = ""
+  rand(2..5).times do
+    desca << Faker::Lorem.sentence
+  end
+
+  queuer = Queuer.new(address: Faker::Address.full_address, description: desca, radius: rand(5...80), price_per_hour: rand(5...40), rating: rand(1..5))
   queuer.user = usere
   queuer.save
 
-  client = User.new(name: Faker::Name.name, email:Faker::Internet.email, password: "password", is_q: false, rating: rand(1..5))
+  descu = ""
+  rand(2..5).times do
+    descu << Faker::Lorem.sentence
+  end
+
+  client = User.new(name: Faker::Name.name, email:Faker::Internet.email, password: "password", is_q: false, rating: rand(1..5), description: descu)
   sleep(2)
   client.remote_picture_url = url
   client.save
