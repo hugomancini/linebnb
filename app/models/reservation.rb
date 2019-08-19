ec4bb93df4f9fcd070f732a937496cb83e242e85
Sami Chenbout <sami.chenbout@essec.edu>
2019-08-19 16:10:44 +0200

    started work on controller

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,4 +1,8 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :queuer
+  validates :starts_at, presence: true
+  validates :ends_at, presence: true
+  validates :content, presence: true
+  validates :adress, presence: true
 end
