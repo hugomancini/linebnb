d03a32646033d8c425aada1aeb052cd6fafe329c
Sami Chenbout <sami.chenbout@essec.edu>
2019-08-19 16:50:27 +0200

    reservation to be reviewed

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,6 +1,7 @@
 class Reservation < ApplicationRecord
   belongs_to :user
   belongs_to :queuer
+  has_many :reviews
   validates :starts_at, presence: true
   validates :ends_at, presence: true
   validates :content, presence: true
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'reservations/index'
-  get 'reservations/show'
-  get 'reservations/new'
-  get 'reservations/create'
-  get 'reservations/destroy'
-  get 'reservations/update'
-  get 'reservations/edit'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
