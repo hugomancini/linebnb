30529f89af9be2ba4df7fe67aac7c7026717a4d6
Hugo MANCINI <hgmancini@gmail.com>
2019-08-19 16:08:05 +0200

    controller + model updated

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :reservations
+  has_many :reviews, through: :reservations
+  validates :email, presence: true, uniqueness: true
+  validates :name, presence: true
 end
