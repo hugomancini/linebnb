10b4dc4a2d52fde3e2a74bc22210d8e338b0f214
Hugo MANCINI <hgmancini@gmail.com>
2019-08-20 11:03:44 +0200

    user.name ok + fullname in sign_up form + before_action authentification in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
+  skip_before_action :authenticate_user!, only: [:home, :queuers]
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
