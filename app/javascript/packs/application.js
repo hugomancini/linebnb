328cc42bf10fc4ea9b0e854fb1b9c8cd67cba4b3
maya douadi <maya.douadi@outlook.fr>
2019-08-22 16:11:32 +0200

    Search autocomplete & queuers map show good

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,7 +3,9 @@ import { modalLogin, modalSignin } from "./modal";
 import 'mapbox-gl/dist/mapbox-gl.css'; // <-- you need to uncomment the stylesheet_pack_tag in the layout!
 import { initMapbox } from '../plugins/init_mapbox';
 import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 initMapbox();
 modalLogin();
 modalSignin();
+initAutocomplete();
